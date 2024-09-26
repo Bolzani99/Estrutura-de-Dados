241fab41ba30ed0987e762b121399f408f46bba6
Rastreamento <rastreamento@menegola.local>
2024-09-25 21:51:57 -0300

    Aula 07

---
+++ Atividades/Aula 07 - Dicionario/Program.cs
@@ -0,0 +1,77 @@
+﻿// Diferente da TabelaHash, o Dictionary nos permite espificicar o tipo de dado que será utilizado nos pares 
+// Kete -> Value 
+// Isso nos forcene uma estrutura fortemente tipada 
+
+
+using Aula;
+
+Dictionary<string, string> dictionary = new Dictionary<string, string>()
+{
+    {"Chave 1", "Valor 1"},
+    {"Chave 2", "Valor 2"},
+};
+
+// Obter valor do dictionary 
+
+string val = dictionary ["Chave 1"];
+
+try
+{
+    val = dictionary["Chave 3"];
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro ao Obter valor chave 3");
+    Console.WriteLine(ex.Message);
+}
+
+// Veficando se a chave existe antes de acessar para evitar erros e prejuizos de excuções 
+
+if (dictionary.ContainsKey("Chave 2") )
+{
+        Console.WriteLine($"Chave 2: {dictionary["Chave 2"]}");
+
+}
+
+// Outra forma de obter o valor e evitar erro 
+dictionary.TryGetValue("Chave 4", out string? valor);
+if(valor is not null)
+    Console.WriteLine($"Chave 4: {valor}");
+
+    //Podemos adicionar da mesma forma pela chave inexstente 
+dictionary["Chave 0"] = "Valor 0";
+
+// Percorrer o dicionário
+
+foreach(KeyValuePair<string, string> kvp in dictionary)
+{
+    Console.WriteLine($"{kvp.Key}:{kvp.Value}"); 
+}
+
+// Utilizando Tipos Abstrator de dados com dicionário 
+
+Dictionary<int, Pessoa> dicPessoa = new Dictionary<int, Pessoa>();
+
+dicPessoa.Add(1, new Pessoa(){
+    Id = 0, 
+    BirthDate = new DateTime(2003, 01, 05),
+    Name = "Silvio Bolzani",
+    
+    }
+);
+
+dicPessoa.Add(2, new Pessoa(){
+    Id = 0, 
+    BirthDate = new DateTime(2019, 07, 20),
+    Name = "Jake",
+    
+    }
+);
+
+foreach(KeyValuePair<int, Pessoa> kvp in dicPessoa)
+{
+    Console.WriteLine(kvp.Key);
+    Console.WriteLine(kvp.Value.Id);
+    Console.WriteLine(kvp.Value.Name);
+    Console.WriteLine(kvp.Value.BirthDate);
+}
---
+++ Atividades/Aula 07 - Tabela Hash/Program.cs
@@ -0,0 +1,42 @@
+﻿// Exemplo de uma agenda telefônica 
+
+
+using System.Collections;
+
+Hashtable phoneBook = new Hashtable()
+{
+    // { "Chave", "Valor" },
+    { "Silvio", "49-99830-0705" },
+    { "Zoe", "49-2222-2222" },
+    { "Jake", "49-1111-1111" },
+};
+
+// É possivel adicionar elementos de diversas formas, pelo proprio indice chave inexistente
+
+phoneBook["Joao"] = "49-99918-8470";
+
+//Ou pelo método add()
+
+phoneBook.Add("Zo", "49-1111-2222");
+
+//Entretando, a TabelaHash verifica se há duplicidade de chave e pode lançar uma Exception (excessão/erro)
+
+try
+{
+    phoneBook.Add("Jake", "49-1111-1111");
+}catch(Exception ex)
+{
+    Console.WriteLine($"Erro ao tentar incluir valor repetido.");
+    Console.WriteLine(ex.Message);
+}
+
+//Percorrendo Itens da HashTable
+
+Console.WriteLine("Agenda Telefônica:");
+if(phoneBook.Count == 0)
+
+    Console.WriteLine("A agenda está vazia.");
+else
+    foreach(DictionaryEntry entry in phoneBook)
+        Console.WriteLine($"{entry.Key}:{entry.Value}");
+    
